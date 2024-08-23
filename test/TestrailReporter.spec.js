1884eec22e71b7fa2674599c7abe0b895964e00e
bun913 <nagasima.romance+@gmail.com>
2024-08-23 23:37:24 +0900

    feat: add vitest

--- test/TestrailReporter.spec.js
+++ test/TestrailReporter.spec.js
@@ -1,12 +1,89 @@
-import { describe, expect, it } from "vitest";
-import TestrailReporter from "../lib/TestrailReporter";
+import { afterEach, describe, expect, it, vi } from "vitest"
+import { makeSampleEmitter } from "./utils/emitter"
+import TestrailReporter from "../lib/TestrailReporter"
 
 describe("TestrailReporter", () => {
-  describe("constructor", () => {
-    it("raise error when domain option is missing", () => {
-      expect(() => {
-        new TestrailReporter();
-      }).toThrowError("A required environment variable domain was not found.");
-    });
-  });
-});
+  describe("onComplete", () => {
+    describe("raise error when required options are missing", () => {
+      afterEach(() => {
+        vi.restoreAllMocks()
+        vi.unstubAllEnvs()
+      })
+      it("raise error when domain option is missing", () => {
+        // arrange
+        const erroSpy = vi.spyOn(console, "error")
+        vi.stubEnv("TESTRAIL_USERNAME", "username")
+        vi.stubEnv("TESTRAIL_APIKEY", "apikey")
+        vi.stubEnv("TESTRAIL_PROJECTID", "projectid")
+        const sut = new TestrailReporter(makeSampleEmitter(vi.fn()), {}, {})
+
+        // act
+        sut.onComplete()
+
+        // assert
+        expect(erroSpy).toHaveBeenCalledWith(
+          expect.stringContaining(
+            "A required environment variable domain was not found.",
+          ),
+        )
+      })
+
+      it("raise error when username option is missing", () => {
+        // arrange
+        const erroSpy = vi.spyOn(console, "error")
+        vi.stubEnv("TESTRAIL_DOMAIN", "domain")
+        vi.stubEnv("TESTRAIL_APIKEY", "apikey")
+        vi.stubEnv("TESTRAIL_PROJECTID", "projectid")
+        const sut = new TestrailReporter(makeSampleEmitter(vi.fn()), {}, {})
+
+        // act
+        sut.onComplete()
+
+        // assert
+        expect(erroSpy).toHaveBeenCalledWith(
+          expect.stringContaining(
+            "A required environment variable username was not found.",
+          ),
+        )
+      })
+
+      it("raise error when apikey option is missing", () => {
+        // arrange
+        const erroSpy = vi.spyOn(console, "error")
+        vi.stubEnv("TESTRAIL_DOMAIN", "domain")
+        vi.stubEnv("TESTRAIL_USERNAME", "username")
+        vi.stubEnv("TESTRAIL_PROJECTID", "projectid")
+        const sut = new TestrailReporter(makeSampleEmitter(vi.fn()), {}, {})
+
+        // act
+        sut.onComplete()
+
+        // assert
+        expect(erroSpy).toHaveBeenCalledWith(
+          expect.stringContaining(
+            "A required environment variable apikey was not found.",
+          ),
+        )
+      })
+
+      it("raise error when projectid option is missing", () => {
+        // arrange
+        const erroSpy = vi.spyOn(console, "error")
+        vi.stubEnv("TESTRAIL_DOMAIN", "domain")
+        vi.stubEnv("TESTRAIL_USERNAME", "username")
+        vi.stubEnv("TESTRAIL_APIKEY", "apikey")
+        const sut = new TestrailReporter(makeSampleEmitter(vi.fn()), {}, {})
+
+        // act
+        sut.onComplete()
+
+        // assert
+        expect(erroSpy).toHaveBeenCalledWith(
+          expect.stringContaining(
+            "A required environment variable projectId was not found.",
+          ),
+        )
+      })
+    })
+  })
+})
---
+++ test/utils/emitter.js
@@ -0,0 +1,5 @@
+export const makeSampleEmitter = (fn) => {
+  return {
+    on: fn,
+  }
+}
