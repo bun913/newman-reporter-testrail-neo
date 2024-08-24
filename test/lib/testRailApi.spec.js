ca802b989f8844db775d62a8cc5161246c7c25b3
bun913 <nagasima.romance+@gmail.com>
2024-08-25 06:10:22 +0900

    chore: run format

--- test/lib/testRailApi.spec.js
+++ test/lib/testRailApi.spec.js
@@ -1,63 +1,65 @@
-import { afterEach, describe, expect, it, vi } from "vitest";
-import { makeFakeRequest, makeFakeTestRailApi } from "../utils/request";
-import { setEnvVars } from "../utils/env";
+import { afterEach, describe, expect, it, vi } from "vitest"
+import { setEnvVars } from "../utils/env"
+import { makeFakeRequest, makeFakeTestRailApi } from "../utils/request"
 
 describe("TestRailApi", () => {
   afterEach(() => {
-    vi.resetAllMocks();
-    vi.unstubAllEnvs();
-  });
+    vi.resetAllMocks()
+    vi.unstubAllEnvs()
+  })
 
   describe("getProjectInfo", () => {
     it("sends reqeust to DOMAIN/api/v2/get_project/testProjectId when suiteId is not set", () => {
       // arrange
-      setEnvVars(vi, { projectId: "123" });
+      setEnvVars(vi, { projectId: "123" })
       const fakeRequest = makeFakeRequest(vi)
-      const sut = makeFakeTestRailApi(vi, fakeRequest, { projectId: "123" });
+      const sut = makeFakeTestRailApi(vi, fakeRequest, { projectId: "123" })
 
       // act
-      sut.getProjectInfo();
+      sut.getProjectInfo()
 
       // assert
       expect(sut.request).toHaveBeenCalledWith(
         "GET",
         "https://example.com/index.php?/api/v2/get_project/testProjectId",
-        expect.anything()
+        expect.anything(),
       )
-    });
+    })
 
     it("sends request to DOMAIN/api_v2_get_suite/:suiteId when suiteId is set", () => {
       // arrange
-      setEnvVars(vi, { TESTRAIL_SUITEID: "456", TESTRAIL_PROJECTID: "123" });
+      setEnvVars(vi, { TESTRAIL_SUITEID: "456", TESTRAIL_PROJECTID: "123" })
       const fakeRequest = makeFakeRequest(vi)
-      const sut = makeFakeTestRailApi(vi, fakeRequest);
+      const sut = makeFakeTestRailApi(vi, fakeRequest)
 
       // act
-      sut.getProjectInfo();
+      sut.getProjectInfo()
 
       // assert
       expect(sut.request).toHaveBeenCalledWith(
         "GET",
         "https://example.com/index.php?/api/v2/get_suite/456",
-        expect.anything()
+        expect.anything(),
       )
     })
-  });
+  })
 
   describe("getRuns", () => {
     it("sends request to DOMAIN/api/v2/get_runs/:projectId when suiteId is not set", () => {
       // arrange
       const fakeRequest = makeFakeRequest(vi)
-      const sut = makeFakeTestRailApi(vi, fakeRequest, { TESTRAIL_PROJECTID: "123" });
+      const sut = makeFakeTestRailApi(vi, fakeRequest, {
+        TESTRAIL_PROJECTID: "123",
+      })
 
       // act
-      sut.getRuns();
+      sut.getRuns()
 
       // assert
       expect(sut.request).toHaveBeenCalledWith(
         "GET",
         "https://example.com/index.php?/api/v2/get_runs/123",
-        expect.anything()
+        expect.anything(),
       )
     })
   })
@@ -66,16 +68,16 @@ describe("TestRailApi", () => {
     it("sends request to DOMAIN/api/v2/get_run/:runId", () => {
       // arrange
       const fakeRequest = makeFakeRequest(vi)
-      const sut = makeFakeTestRailApi(vi, fakeRequest);
+      const sut = makeFakeTestRailApi(vi, fakeRequest)
 
       // act
-      sut.getRun("456");
+      sut.getRun("456")
 
       // assert
       expect(sut.request).toHaveBeenCalledWith(
         "GET",
         "https://example.com/index.php?/api/v2/get_run/456",
-        expect.anything()
+        expect.anything(),
       )
     })
   })
@@ -84,48 +86,53 @@ describe("TestRailApi", () => {
     it("sends request to DOMAIN/api/v2/get_cases/:projectId when suiteId is not set", () => {
       // arrange
       const fakeRequest = makeFakeRequest(vi, {
-        "_links": {
-          "next": null
+        _links: {
+          next: null,
         },
         cases: [
           { id: 1, title: "case1" },
           { id: 2, title: "case2" },
-        ]
+        ],
       })
-      const sut = makeFakeTestRailApi(vi, fakeRequest, { TESTRAIL_PROJECTID: "123" });
+      const sut = makeFakeTestRailApi(vi, fakeRequest, {
+        TESTRAIL_PROJECTID: "123",
+      })
 
       // act
-      sut.getCases();
+      sut.getCases()
 
       // assert
       expect(sut.request).toHaveBeenCalledWith(
         "GET",
         "https://example.com/index.php?/api/v2/get_cases/123",
-        expect.anything()
+        expect.anything(),
       )
     })
 
     it("sends request to DOMAIN/api/v2/get_cases/:projectId&suite_id=:suiteId when suiteId is set", () => {
       // arrange
       const fakeRequest = makeFakeRequest(vi, {
-        "_links": {
-          "next": null
+        _links: {
+          next: null,
         },
         cases: [
           { id: 1, title: "case1" },
           { id: 2, title: "case2" },
-        ]
+        ],
       })
-      const sut = makeFakeTestRailApi(vi, fakeRequest, { TESTRAIL_PROJECTID: "123", TESTRAIL_SUITEID: "456" });
+      const sut = makeFakeTestRailApi(vi, fakeRequest, {
+        TESTRAIL_PROJECTID: "123",
+        TESTRAIL_SUITEID: "456",
+      })
 
       // act
-      sut.getCases();
+      sut.getCases()
 
       // assert
       expect(sut.request).toHaveBeenCalledWith(
         "GET",
         "https://example.com/index.php?/api/v2/get_cases/123&suite_id=456",
-        expect.anything()
+        expect.anything(),
       )
     })
 
@@ -133,16 +140,18 @@ describe("TestRailApi", () => {
       it("sends request to DOMAIN/api/v2/add_run/:projectId", () => {
         // arrange
         const fakeRequest = makeFakeRequest(vi)
-        const sut = makeFakeTestRailApi(vi, fakeRequest, { TESTRAIL_PROJECTID: "123" });
+        const sut = makeFakeTestRailApi(vi, fakeRequest, {
+          TESTRAIL_PROJECTID: "123",
+        })
 
         // act
-        sut.addRun("title", []);
+        sut.addRun("title", [])
 
         // assert
         expect(sut.request).toHaveBeenCalledWith(
           "POST",
           "https://example.com/index.php?/api/v2/add_run/123",
-          expect.anything()
+          expect.anything(),
         )
       })
     })
@@ -151,16 +160,18 @@ describe("TestRailApi", () => {
       it("sends request to DOMAIN/api/v2/add_plan_entry/:planId", () => {
         // arrange
         const fakeRequest = makeFakeRequest(vi)
-        const sut = makeFakeTestRailApi(vi, fakeRequest, { TESTRAIL_PLANID: "123" });
+        const sut = makeFakeTestRailApi(vi, fakeRequest, {
+          TESTRAIL_PLANID: "123",
+        })
 
         // act
-        sut.addPlanEntry("title", []);
+        sut.addPlanEntry("title", [])
 
         // assert
         expect(sut.request).toHaveBeenCalledWith(
           "POST",
           "https://example.com/index.php?/api/v2/add_plan_entry/123",
-          expect.anything()
+          expect.anything(),
         )
       })
     })
@@ -169,22 +180,20 @@ describe("TestRailApi", () => {
       it("sends request to DOMAIN/api/v2/close_run/:runId", () => {
         // arrange
         const fakeRequest = makeFakeRequest(vi)
-        const sut = makeFakeTestRailApi(vi, fakeRequest);
+        const sut = makeFakeTestRailApi(vi, fakeRequest)
 
         // act
-        sut.closeRun("456");
+        sut.closeRun("456")
 
         // assert
         expect(sut.request).toHaveBeenCalledWith(
           "POST",
           "https://example.com/index.php?/api/v2/close_run/456",
-          expect.anything()
+          expect.anything(),
         )
       })
     })
   })
 
   // TODO: refactor pushToTestrail and write tests using fakes
-
-
-});
+})
--- test/utils/request.js
+++ test/utils/request.js
@@ -1,18 +1,18 @@
-import { setEnvVars } from "./env";
-import  TestRailApi  from "../../lib/testRailApi";
+import TestRailApi from "../../lib/testRailApi"
+import { setEnvVars } from "./env"
 
-export const makeFakeRequest = (vi, fakeResponse={"fake": true}) => {
+export const makeFakeRequest = (vi, fakeResponse = { fake: true }) => {
   const mockRequest = vi.fn((method, url, options) => {
     return {
       statusCode: 200,
       getBody: () => JSON.stringify(fakeResponse),
-    };
-  });
-  return mockRequest;
-};
+    }
+  })
+  return mockRequest
+}
 
-export const makeFakeTestRailApi = (vi,fakeRequest, envOverrids = {}) => {
+export const makeFakeTestRailApi = (vi, fakeRequest, envOverrids = {}) => {
   // fake TestRailApi
-  setEnvVars(vi, envOverrids);
+  setEnvVars(vi, envOverrids)
   return new TestRailApi(fakeRequest)
-};
+}
