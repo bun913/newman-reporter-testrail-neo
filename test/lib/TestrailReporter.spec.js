0ea0bfd692f8297e1fe767bd1c27399aaa796b22
bun913 <nagasima.romance+@gmail.com>
2024-08-30 23:57:13 +0900

    feat: add smoke test

--- test/lib/TestrailReporter.spec.js
+++ test/lib/TestrailReporter.spec.js
@@ -1,13 +1,13 @@
 import { afterEach, describe, expect, it, vi } from "vitest"
-import TestrailReporter from "../lib/TestrailReporter"
-import getEnv from "../lib/environment"
-import { makeSampleEmitter } from "./utils/emitter"
-import { setEnvVars } from "./utils/env"
+import TestrailReporter from "../../lib/TestrailReporter"
+import getEnv from "../../lib/environment"
+import { makeSampleEmitter } from "../utils/emitter"
+import { setEnvVars } from "../utils/env"
 import {
   makeFakeJsonifyResult,
   makeTestrailReporterWithFakeApi,
-} from "./utils/fake"
-import makeNewmanResult from "./utils/newman"
+} from "../utils/fake"
+import makeNewmanResult from "../utils/newman"
 
 describe("TestrailReporter", () => {
   describe("onComplete", () => {
