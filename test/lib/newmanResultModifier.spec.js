6115fdab5448002dc1eafde68c1aff0d903d3f41
bun913 <nagasima.romance+@gmail.com>
2024-09-01 13:45:26 +0900

    chore: run formatter

--- test/lib/newmanResultModifier.spec.js
+++ test/lib/newmanResultModifier.spec.js
@@ -1,8 +1,8 @@
-import NewmanResultModifier from "../../lib/newmanResultModifier";
-import { afterEach, describe, expect, it, vi } from "vitest";
-import makeNewmanResult from "../utils/newman";
-import { setEnvVars } from "../utils/env";
-import { makeFakeRequest, makeFakeTestRailApi } from "../utils/fake";
+import { afterEach, describe, expect, it, vi } from "vitest"
+import NewmanResultModifier from "../../lib/newmanResultModifier"
+import { setEnvVars } from "../utils/env"
+import { makeFakeRequest, makeFakeTestRailApi } from "../utils/fake"
+import makeNewmanResult from "../utils/newman"
 
 const makeFakeApi = (vi, fakeResponse, env) => {
   const defaultResponse = {
@@ -13,45 +13,47 @@ const makeFakeApi = (vi, fakeResponse, env) => {
       { id: 1, title: "case1" },
       { id: 2, title: "case2" },
     ],
-  };
-  const fakeRequest = makeFakeRequest(vi, fakeResponse || defaultResponse);
-  return makeFakeTestRailApi(vi, fakeRequest, env);
-};
+  }
+  const fakeRequest = makeFakeRequest(vi, fakeResponse || defaultResponse)
+  return makeFakeTestRailApi(vi, fakeRequest, env)
+}
 
 describe("NewmanResultModifier", () => {
   afterEach(() => {
     vi.unstubAllEnvs()
-    vi.resetAllMocks();
-  });
+    vi.resetAllMocks()
+  })
 
   describe("addTestCaseIdsToAssertions", () => {
     it("nothing changes if isUseTitle is not true", () => {
       // arrange
-      const fakeApi = makeFakeApi(vi, null, {});
-      const executions = makeNewmanResult();
+      const fakeApi = makeFakeApi(vi, null, {})
+      const executions = makeNewmanResult()
       const sut = new NewmanResultModifier("false", fakeApi, executions)
       // act
-      const result = sut.addTestCaseIdsToAssertions();
+      const result = sut.addTestCaseIdsToAssertions()
       // assert
-      expect(result).toEqual(executions);
-    });
+      expect(result).toEqual(executions)
+    })
 
     it("addTestCaseIds to assertion titles", () => {
-        // arrange
-        const fakeApi = makeFakeApi(vi, null, {})
-        const executions = makeNewmanResult({
-            assertionName: "case1"
-        })
-        const multiCaseExecutions = executions.concat(makeNewmanResult({
-            assertionName: "case2"
-        }))
-        const sut = new NewmanResultModifier("true", fakeApi, multiCaseExecutions)
+      // arrange
+      const fakeApi = makeFakeApi(vi, null, {})
+      const executions = makeNewmanResult({
+        assertionName: "case1",
+      })
+      const multiCaseExecutions = executions.concat(
+        makeNewmanResult({
+          assertionName: "case2",
+        }),
+      )
+      const sut = new NewmanResultModifier("true", fakeApi, multiCaseExecutions)
 
-        // act
-        const results = sut.addTestCaseIdsToAssertions()
-        // assert
-        expect(results[0].assertions[0].assertion).toEqual("C1 case1")
-        expect(results[1].assertions[0].assertion).toEqual("C2 case2")
+      // act
+      const results = sut.addTestCaseIdsToAssertions()
+      // assert
+      expect(results[0].assertions[0].assertion).toEqual("C1 case1")
+      expect(results[1].assertions[0].assertion).toEqual("C2 case2")
     })
-  });
-});
+  })
+})
